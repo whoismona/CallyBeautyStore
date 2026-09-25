9251286e57d05e59082109ee49320df22e5f2a4f
Emma-Drumz <alfaonura@gmail.com>
2026-09-25 20:12:32 +0100

    update website

--- main.js
+++ main.js
@@ -11,3 +11,29 @@ menuBtn.addEventListener('click', function() {
 	}
 	
 });
+function viewProduct(image) {
+    document.getElementById("modalImage").src = image;
+    document.getElementById("productModal").style.display = "flex";
+}
+
+function closeProduct() {
+    document.getElementById("productModal").style.display = "none";
+}
+function sendWhatsApp() {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let text = `Hello CallyBeautyStore!
+
+Name: ${name}
+Email: ${email}
+Message:
+${message}`;
+
+    let whatsappNumber = "2347065723958";
+
+    let whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(text)}`;
+
+    window.open(whatsappURL, "_blank");
+}
